e025870ca1cd5715ab405c9d1101bb848bb2c380
Foot <phuvo2105@gmail.com>
2021-06-15 14:35:17 +0700

    Tach layout va sua lai logic phan lay du lieu tu database

--- apps/controllers/index.js
+++ apps/controllers/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
+const post_md = require("../models/post")
+
 router.use("/admin", require(__dirname + "/admin.js"));
 router.use("/blog", require(__dirname + "/blog.js"));
 router.use("/updateuserinfo", require(__dirname + "/updateuserinfo.js"))
@@ -19,21 +21,43 @@ router.post('/signout', (req, res) => {
 
 router.get("/", function(req, res){
     if(req.session.user){
+        const data = post_md.getAllPosts();
+
         const _user = req.session.user;
 
-        const data ={
-            user: {
-                first_name: _user.first_name,
-                last_name: _user.last_name,
-                email: _user.email,
-                address1: _user.address1,
-                address2: _user.address2,
-                state: _user.state,
-                country: _user.country,
-                phone: _user.phone
+        data.then(function(posts){
+            let avatarpath;
+
+            if(_user.imgpath){
+                const rawimgpath = _user.imgpath;
+                avatarpath =rawimgpath.substring(rawimgpath.indexOf('img'));
             }
-        };
-        res.render("home", data)
+            else{
+                avatarpath = ''
+            }
+
+            let avatarimgpath = avatarpath.replace(/\\/g, "/")
+
+            const data = {
+                user: {
+                    first_name: _user.first_name,
+                    last_name: _user.last_name,
+                    email: _user.email,
+                    address1: _user.address1,
+                    address2: _user.address2,
+                    state: _user.state,
+                    country: _user.country,
+                    phone: _user.phone,
+                    imgpath: avatarimgpath,
+                    company_name: _user.company_name,
+                    company_site: _user.company_site
+                }  
+            };
+
+            res.render("home", data)
+        }).catch(function(err){
+            res.json({error: true, message: err.message});
+        });
     }
     else {
         res.redirect("/admin/signin");
